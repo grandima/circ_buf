982481a6bdd86985803809b322cb099bf30f4dd3
Dmytro Medynsky <grandima@gmail.com>
2016-06-03 10:56:12 +0300

    Initial Commit

---
+++ circ_buf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  circ_buf
+//
+//  Created by dmytro.medynsky on 6/3/16.
+//  Copyright © 2016 dmytro.medynsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
